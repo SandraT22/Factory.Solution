c09edcbbe4d6881f228ee98ab3bfa2e1bfbdff36
Sandra Tena <sandratena06@gmail.com>
2022-08-05 10:51:35 -0700

    add models

--- Factory/Models/Engineer.cs
+++ Factory/Models/Engineer.cs
@@ -1,12 +1,17 @@
-// using System.Collections.Generic;
+using System.Collections.Generic;
 
-// namespace Factory.Models
-// {
-//   public class Engineer
-//   {
-//     public Engineer()
-//     {
-     
-//     }
-//   }
-// }
+namespace Factory.Models
+{
+  public class Engineer
+  {
+    public Engineer()
+    {
+      this.JoinEntities = new HashSet<EngineerMachine>();
+    }
+
+    public int EngineerId { get; set; }
+    public string Name { get; set; }
+    public string Contact { get; set; }
+    public virtual ICollection<EngineerMachine> JoinEntities { get; set; }
+  }
+}
